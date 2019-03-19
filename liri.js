5d24294ae5c6f244b5f5a9fb78638938cd94b945
atiftariq786 <atiftariq786@hotmail.com>
2019-03-19 12:03:23 -0700

    Modified liri.js

--- liri.js
+++ liri.js
@@ -5,7 +5,7 @@
 
 
 var axios = require("axios");
-
+var moment = require('moment');
 
 
 // Grab the movieName 
@@ -31,9 +31,40 @@ switch (action){
 
 }
 
+function artist(artistEvents) {
 
+  if(artistEvents === undefined ){
+    artistEvents = "Drake";
+  }
 
+  
+  
+  var queryUrl = "https://rest.bandsintown.com/artists/" + artistEvents + "/events?app_id=89edd9fd-9632-4b49-b6bf-d77e59aee2fd";
+  
+  
+  console.log(queryUrl);
+  
+  axios.get(queryUrl).then(
+    function(response) {
 
+      var apiresponse = response.data;
+     // console.log(apiresponse);
+           
+      console.log("Venue Name: " + apiresponse[0].venue.name);
+      console.log("Location Country: " + apiresponse[0].venue.country);
+      console.log("Location City: " + apiresponse[0].venue.city);
+
+      // time conversion (MM/DD/YYY) using moment 
+      var dateTime = moment(apiresponse[0].datetime).format("MM, DD, YYYY, h:mm a");
+      console.log("Event Date: " + dateTime);
+  
+      
+           
+    
+    });
+  
+  };
+  
 
 function movie(movieName) {
 
@@ -50,7 +81,7 @@ console.log(queryUrl);
 
 axios.get(queryUrl).then(
   function(response) {
-    console.log(response.data);
+    //console.log(response.data);
     console.log("Movie Title: " + response.data.Title);
     console.log("Release Year: " + response.data.Year);
     console.log("ImdbRating: " + response.data.imdbRating);
